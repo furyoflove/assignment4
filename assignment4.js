898dee7f152c5e7ca244f2b6645f3e9899edc26a
Francis <fransterx123@gmail.com>
2017-04-12 21:20:36 -0400

    completed

--- assignment4.js
+++ assignment4.js
@@ -12,6 +12,43 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
+
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+
+  // on keyboard press, trigger the predictive search
+  $('.flexsearch-input').keyup(function(e) {
+
+      var input = $('.flexsearch-input').val().toLowerCase();
+      $('.flexsearch-predictions').empty();
+
+     	// find the dictionary of predictive search items
+     	var jqxhr = $.getJSON(" http://www.mattbowytz.com/simple_api.json?data=all", function( data ) {
+
+  		 	for (var row in data) {
+  				for (var col in data[row]) {
+  					var possibleResults = data[row][col]; // predictive search items
+  					for( var index in possibleResults) {
+  						var current = possibleResults[index].toLowerCase();
+  						if( current.startsWith(input) && input.length > 0 && current.length > 1) {
+               			$('.flexsearch-predictions').append('<a href=https://www.google.com/#q=' + current + ' target="_blank">' + current + '</a><br>');
+              }
+            } // innermost for
+          } // second for
+  			} // outermost for
+
+ 		   })
+       .done( function() {
+         console.log("getJSON request successful. ")
+       })
+       .fail( function( jqxhr ) {
+          console.log("getJSON request failed: " + jqxhr);
+       })
+       .always( function() {
+         console.log("getJSON request attempted. ");
+       });
+       // End of JSON get
+
+  }); // End of keyboard event handler
+
 })();
